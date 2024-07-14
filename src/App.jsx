66ac7eb614b381a36860c7c6c478f66c6a72f495
Adriana Maretic <adriana.maretic123@gmail.com>
2024-07-14 10:27:27 +0200

    negative input values and duration===0 fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import { InputGroup } from "./components/InputGroup";
 import { Results } from "./components/Results";
 
 const initialInputValues = {
-  initialInvestment: null,
-  annualInvestment: null,
-  expectedReturn: null,
-  duration: null,
+  initialInvestment: 0,
+  annualInvestment: 0,
+  expectedReturn: 0,
+  duration: 0,
 };
 
 function App() {
   const [inputValues, setInputValues] = useState(initialInputValues);
+
+  const inputIsValid =
+    inputValues.duration > 0 &&
+    inputValues.initialInvestment >= 0 &&
+    inputValues.annualInvestment >= 0 &&
+    inputValues.expectedReturn >= 0;
 
   function handleInputChange(data) {
     const changedInputId = data.target.id;
@@ -25,11 +31,11 @@ function App() {
     setInputValues(changedValues);
   }
 
-
   return (
     <main>
       <InputGroup handleChange={handleInputChange}></InputGroup>
-      <Results inputChanges={inputValues}></Results>
+      {!inputIsValid && <p className="center">Please enter valid data</p>}
+      {inputIsValid && <Results inputChanges={inputValues}></Results>}
     </main>
   );
 }
